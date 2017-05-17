281f9e5cd6d9f5343398af5b949c16d5fd1b85ad
Parnell Springmeyer <parnell@digitalmentat.com>
2017-05-17 12:41:23 -0500

    Relaxing dep bounds on optparse-applicative

--- config.nix
+++ config.nix
@@ -2,10 +2,6 @@
   packageOverrides = pkgs: {
     haskellPackages = pkgs.haskellPackages.override {
       overrides = haskellPackagesNew: haskellPackagesOld: {
-        optparse-applicative =
-          pkgs.haskell.lib.dontCheck
-            (haskellPackagesNew.callPackage ./nix/optparse-applicative.nix { });
-
         optparse-generic =
           haskellPackagesNew.callPackage ./nix/optparse-generic.nix { };
 
